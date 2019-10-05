27a2e1a2d30d20e9df0c07fc292acb83a253c58b
christineKarimi <karimikim3@gmail.com>
2019-10-05 22:00:04 +0300

    Function that gets the difference of the sum of squares.

--- sumsqdifference.py
+++ sumsqdifference.py
@@ -23,3 +23,18 @@ def sum_of_square(limit):
         if len(my_list)==limit:
             m = sum(my_list)
     return m
+
+    def square_of_sum(limit):
+    list = []
+    for a in range(1,limit +1):
+        list.append(a)
+        p = sum(list)
+        if len(list)==limit:
+		continue
+    return p*p
+
+    def difference(limit):
+    num_sum = square_of_sum(limit)-sum_of_square(limit)
+    return num_sum
+
+difference(10)
