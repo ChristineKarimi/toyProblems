8d63a1bf98b9cc49086bfc611cd400694d97f2de
christineKarimi <karimikim3@gmail.com>
2019-10-05 22:07:12 +0300

    Defined isPrime function that checks if a numnber is divisible by another number.

---
+++ sumofprime.py
@@ -0,0 +1,28 @@
+# PROBLEM 10
+# -----------
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+# Find the sum of all the primes below two million.
+
+# SOLUTION
+# ---------
+def is_prime(n):
+	#runs as per O(1)
+	#check if number if lesser or equal to one
+    if n <= 1:
+        return False
+	#check if number is equal to two, return True since two is a prime
+    if n == 2:
+        return True
+	#check if number is greater than two, that is start from three and check if number is even(even numbers cannot be prime)
+    if n > 2 and n % 2 == 0:
+        return False
+ 	#find the square root(**) of the number then find the whole number( int() ) of the square root
+    max_sqrt = int(n**0.5)
+	
+	#loop through  the number and check if the number is divisible by another number
+    for i in range(3, 1 + max_sqrt, 2):
+        if n % i == 0:
+            return False
+	#return True if the number is prime
+    return True
